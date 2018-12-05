16dbdc2a59e95e2fa3a28bb751c5c3345e3d6990
Пользователь <mdylll@mail.ru>
2018-12-05 15:50:48 +0300

    relis 9

--- src/main3.cpp
+++ src/main3.cpp
@@ -5,10 +5,9 @@
 int main(void)
 {
 	int line, count;
-	char *buf;
+	char buf[100];
 	printf("Input number of lines: ");
 	scanf("%d", &count);
-	buf = (char*)malloc(count * 2 * sizeof(char));
 	for (line = 1; line <= count; line++)
 		printf("%s\n", layout(buf, line, count));
 	return 0;
