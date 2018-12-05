462cabf06f5969cef9279b844a4f0a98ffe0c427
Пользователь <mdylll@mail.ru>
2018-12-05 15:06:44 +0300

    relis 5

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,6 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <Windows.h>
+#include <time.h>
 #include "task1.h"
 
 int main(void)
@@ -13,7 +13,8 @@ int main(void)
 	while (height(t, startHeight) > 0)
 	{
 		printf("t=%02d c h=%2.1f m\n", t, height(t, startHeight));
-		Sleep(1000);
+		unsigned int retTime = time(0) + 1;
+		while (time(0) < retTime);
 		t++;
 	}
 	printf("BABAH!!!");
--- src/task5.cpp
+++ src/task5.cpp
@@ -1,15 +1,7 @@
 #include <time.h>
 #include <stdlib.h>
-#include <Windows.h>
 
 
-long long int microseconds() //попытаемся внести случайность микросекундами
-{
-	LARGE_INTEGER t;
-	QueryPerformanceCounter(&t);
-	Sleep(50); //за 50 000 микросекунд что-нибудь нарушится в единицах
-	return t.QuadPart;
-}
 
 char * password(char * line)
 {
@@ -17,25 +9,25 @@ char * password(char * line)
 	long long int t;
 	for (i = 0; i < 3; i++) //добавим цифры
 	{
-		t = microseconds();
+		t = rand();
 		j = 48 + t % 10;
 		line[i] = char(j);
 	}
 	for (i = 3; i < 6; i++) //добавим буквы
 	{
-		t = microseconds();
+		t = rand();
 		j = 65 + t % 26;
 		line[i] = char(j);
 	}
 	for (i = 6; i < 8; i++) //добавим буквы другого размера
 	{
-		t = microseconds();
+		t = rand();
 		j = 97 + t % 26;
 		line[i] = char(j);
 	}
 	for (i = 0; i < 10; i++) //перемешаем
 	{
-		t = (microseconds()/1000) % 8;
+		t = rand() % 8;
 		buf = line[t];
 		line[t] = line[7 - t];
 		line[7 - t] = buf;
