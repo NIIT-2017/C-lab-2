fb5baec4123057014fd45231d2c563a35930b135
krostis <krostis77@mail.ru>
2018-12-09 00:52:19 +0300

    1.3

--- src/task1.cpp
+++ src/task1.cpp
@@ -7,14 +7,13 @@ float height(int currTime, int startHeight)
 {
 	for (float height_t = 0; height_t >= 0; currTime++)
 	{
-		int plus = time (NULL) + 1;
-		while (time(NULL) < plus);
-
 		height_t = startHeight - 9.81*(currTime*currTime) / 2;
 				if (height_t >= 0)
-			printf("t=%02d c   h=%.1f m\n", currTime, height_t);
+					printf("t=%02d c   h=%.1f m\n", currTime, height_t);
 		else 
 			printf("BABAH!!!");
+		int plus = time(NULL) + 1;
+		while (time(NULL) < plus);
 	}
 
 	return 0;
