4e51d646246cdb0da12fa59e22945ad244c2c350
krostis <krostis77@mail.ru>
2018-12-09 19:01:45 +0300

    1.3

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,17 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <time.h>
 #include "task1.h"
 
 int main()
 {
-	int startHeight = 0;
+	int startHeight;
 	int currTime = 0;
 	
 	printf("Enter height: \n");
 	scanf("%d", &startHeight);
-	height(currTime, startHeight);
+
+	for (; height(currTime, startHeight) > 0; currTime++)
+	{
+		if (height(currTime, startHeight) > 0)
+			printf("t=%02d c   h=%.1f m\n", currTime, height(currTime, startHeight));
+		int plus = time(NULL) + 1;
+		while (time(NULL) < plus);
+	}
+	
+	printf("BABAH!!!");
 
 	getchar();
 	getchar();
 	return 0;
 }
+
--- src/task1.cpp
+++ src/task1.cpp
@@ -1,20 +1,6 @@
-#include <stdio.h>
-#include <time.h>
-
-
-
 float height(int currTime, int startHeight)
 {
-	for (float height_t = 0; height_t >= 0; currTime++)
-	{
-		height_t = startHeight - 9.81*(currTime*currTime) / 2;
-				if (height_t >= 0)
-					printf("t=%02d c   h=%2.1f m\n", currTime, height_t);
-		else 
-			printf("BABAH!!!");
-		int plus = time(NULL) + 1;
-		while (time(NULL) < plus);
-	}
-
-	return 0;
+	float height_t = startHeight - 9.81*(currTime*currTime) / 2;
+	return height_t;
 }
+
