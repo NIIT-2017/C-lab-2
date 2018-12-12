b39071a8850f92733dc90a82365372d61299f0f7
krostis <krostis77@mail.ru>
2018-12-12 03:19:16 +0300

    1.3

--- src/task6.cpp
+++ src/task6.cpp
@@ -3,23 +3,16 @@
 
 char * clear(char * line)
 {
-	int begin = 0, i=0;
-	void shift(int begin, char line);
-	
+	int begin = 0, i = 0;
+
 	while (line[begin] == ' ')
 		for (i = begin; i < strlen(line); i++)
 			line[i] = line[i + 1];
 
-	begin++;
 	for (; begin < strlen(line); begin++)
 		while (line[begin] == ' '&& line[begin + 1] == ' ')
 			for (i = begin; i < strlen(line); i++)
 				line[i] = line[i + 1];
 
-	int q = strlen(line);
-	if (line[q - 2] == ' ')
-		line[q - 2] = '\0';
-	
-	
 	return line;
 }
