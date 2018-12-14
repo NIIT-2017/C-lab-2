a051bd75897a3b920f3255c22cc5406355ad4ab1
alekseypivovar <you@example.com>
2018-12-14 23:27:56 +0300

    Release n+2

--- src/task6.cpp
+++ src/task6.cpp
@@ -6,37 +6,39 @@ char * clear(char * buf)
 	int spaceCounter = 0;
 	char buffer;
 
-	for (int i = 0; buf[i] != 0; i++)
+	while (buf[0] == ' ')
+	{
+		for (int i = 1; i <= strlen(buf); i++)
+			{
+				buffer = buf[i];
+				buf[i - 1] = buffer;
+			}
+	}
+	if (buf[strlen(buf) - 1] == '\n')
+		buf[strlen(buf) - 1] = '\0';
+
+	for (int i = 0; buf[i] != '\0'; i++)
 	{
 		if (buf[i] == ' ')
 			spaceCounter++;
-		if (buf[i] != ' ' && spaceCounter>=1)
+		else if (buf[i] != ' ' && spaceCounter > 1)
 		{
-			int j = i - (spaceCounter - 1);
-			for (; buf[j] != 0; j++)
+			int j = i;
+			for (; j<=strlen(buf); j++)
 			{
-				buffer = buf[j+ (spaceCounter - 1)];
-				buf[j] = buffer;
+				buffer = buf[j];
+				buf[j - (spaceCounter - 1)] = buffer;
 			}
+			i = i - (spaceCounter - 1);
 			spaceCounter = 0;
-			buf[j] = '\0';
 		}
-	}
-
-
-	if (buf[0] == ' ')
-	{
-		for (int i = 1; buf[i] != 0; i++)
-		{
-			buffer = buf[i];
-			buf[i - 1] = buffer;
-			buf[i] = '\0';
-		}
-	}
-
-	if (buf[(strlen(buf) - 1)]==' ')
-	buf[(strlen(buf) - 1)] = '\0';
+		else if (buf[i] == '\0' && spaceCounter > 0)
+			buf[i- spaceCounter]='\0';
+		else if (buf[i] != ' ' && spaceCounter == 1)
+			spaceCounter = 0;
 	
+	}
+
+
 	return buf;
 }
-
