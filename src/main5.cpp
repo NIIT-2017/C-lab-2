c92dc906ded0e01c3d7de2e01d792c59ecb9b80a
DmitTitov <dm_titov@list.ru>
2018-12-16 18:24:56 +0300

    Lab2 v4

--- src/main5.cpp
+++ src/main5.cpp
@@ -1,11 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 #include "task5.h"
 
 int main()
-{
+{	
+	srand(time(0));
 	char  line[9] = { 0 };
 	printf("Password Generator\n");
-	password(line);
+
+	for (int j = 0; j < 10; j++)   // в этом цикле создаются 10 паролей
+	{
+		printf("%s\n", password(line));
+	}
+
 	return 0;
 }
