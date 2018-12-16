073d4508828aca632b0f9d060c13676bbfe9a288
DmitTitov <dm_titov@list.ru>
2018-12-16 18:00:23 +0300

    Lab2 v3

--- src/main7.cpp
+++ src/main7.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 #define SIZEBUF 256
 
 int main()
--- src/task5.cpp
+++ src/task5.cpp
@@ -1,5 +1,6 @@
 #include "task5.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 char * password(char * line)
