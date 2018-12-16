30c8704e52650fa50efc15913bc117d10bfc838a
marisergeewna1986 <marisergeewna@mail.ru>
2018-12-16 20:29:12 +0300

    task3

--- src/task3.cpp
+++ src/task3.cpp
@@ -3,9 +3,9 @@
 char * layout(char buf[], int line, int count)
 {
 	int j;
-	for (j = 0; j <= (count + line); j++)
+	for (j = 0; j <= (count + line-2); j++)
 	{
-		if (j <= (count - line + 1))
+		if (j < (count - line))
 			buf[j] = ' ';
 		else
 			buf[j] = '*';
