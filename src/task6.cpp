44edd6a196b2bc2fcc8c7d8ea0c5248ab8f20102
alekseypivovar <you@example.com>
2018-12-18 00:33:39 +0300

    Release n+3

--- src/task6.cpp
+++ src/task6.cpp
@@ -32,13 +32,12 @@ char * clear(char * buf)
 			i = i - (spaceCounter - 1);
 			spaceCounter = 0;
 		}
-		else if (buf[i] == '\0' && spaceCounter > 0)
-			buf[i- spaceCounter]='\0';
-		else if (buf[i] != ' ' && spaceCounter == 1)
+			else if (buf[i] != ' ' && spaceCounter == 1)
 			spaceCounter = 0;
 	
 	}
-
+	if (buf[strlen(buf)-1] == ' ')
+		buf[strlen(buf) - 1] = '\0';
 
 	return buf;
 }
