a535cfc7bfcfff56274abc92886ec72462500750
alekseypivovar <you@example.com>
2018-12-18 05:04:50 +0300

    The last release I hope

--- src/task6.cpp
+++ src/task6.cpp
@@ -5,46 +5,41 @@
 char * clear(char * buf)
 {
 	int spaceCounter = 0;
-	char buffer;
+
+	if (buf[strlen(buf) - 1] == '\n')
+		buf[strlen(buf) - 1] = '\0';
 
 	while (buf[0] == ' ')
 	{
 		for (int i = 1; i <= strlen(buf); i++)
 			{
-				buffer = buf[i];
-				buf[i - 1] = buffer;
+				buf[i - 1] = buf[i];
 			}
 	}
 	
-
-	for (int i = 0; buf[i] != '\0'; i++)
+	for (int i = 0; i <= strlen(buf); i++)
 	{
 		if (buf[i] == ' ')
 			spaceCounter++;
+		else if (buf[i] == '\0' && spaceCounter > 0)
+			buf[i - spaceCounter] = '\0';
 		else if (buf[i] != ' ' && spaceCounter > 1)
 		{
 			int j = i;
-			for (; j<=strlen(buf); j++)
+			for (; j <= strlen(buf); j++)
 			{
-				buffer = buf[j];
-				buf[j - (spaceCounter - 1)] = buffer;
+				buf[j - (spaceCounter - 1)] = buf[j];
+				if (buf[j] == '\0')
+				{
+					i = i - (spaceCounter - 1);
+					spaceCounter = 0;
+				}
 			}
-			i = i - (spaceCounter - 1);
-			spaceCounter = 0;
+			
 		}
-			else if (buf[i] != ' ' && spaceCounter == 1)
+		else if (buf[i] != ' ' && spaceCounter == 1)
 			spaceCounter = 0;
 
-			else if (buf[i] == '\0' && spaceCounter > 0)
-			buf[i - spaceCounter] = '\0';
-	
 	}
-	for (int i = 0; i <= strlen(buf); i++)
-	{
-		if (buf[i] == '\n')
-			buf[i] = '\0';
-	}
-		
-
 	return buf;
 }
