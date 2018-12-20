c37b5f805362b708273435bd9fc04097a16c9eef
Nikolai <44969889+nbuzynin@users.noreply.github.com>
2018-12-20 15:29:54 +0300

    Исправления в пятой задаче

--- src/main5.cpp
+++ src/main5.cpp
@@ -7,6 +7,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "task5.h"
 
 const int N = 10, pass_length = 8;		//n - количество паролей
@@ -14,10 +15,8 @@ const int N = 10, pass_length = 8;		//n - количество паролей
 int main()
 {
 	char line[N] = { 0 };
+	srand(time(NULL));
 	for (int i = 1; i <= N; i++)
-	{
 		puts(password(line));
-		printf("\n");
-	}
 	return 0;
 }
--- src/task5.cpp
+++ src/task5.cpp
@@ -1,21 +1,15 @@
 #include <stdlib.h>
-#include <time.h>
-#include <windows.h>
 #include "task5.h"
 
 extern const int pass_length;
 
 char * password(char * line)
 {
-	srand(time(NULL));
 	for (int i = 0; i < pass_length; i++)
 	{
 		do
-		{
 			line[i] = 48 + rand() % (122 - 48 + 1);
-			//Sleep(110);
-		}
-			while ((line[i] >= 58 && line[i] <= 64) || (line[i] >= 91 && line[i] <= 96));
+		while ((line[i] >= 58 && line[i] <= 64) || (line[i] >= 91 && line[i] <= 96));
 	}
 	return line;
 }
