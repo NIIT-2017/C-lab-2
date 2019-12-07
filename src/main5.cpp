3405ddf935cdee60e9ce9e931333f0859a7e314a
asintsov <57562296+asintsov@users.noreply.github.com>
2019-12-07 13:29:39 +0300

    Update main5.cpp

--- src/main5.cpp
+++ src/main5.cpp
@@ -6,19 +6,14 @@
 
 int main()
 {
-    char line[128] = { 0 };
+    char line[N] = { 0 };
+    
     time_t now;
     time(&now);
     srand(now);
-    rand();
-    
+       
     for (int k = 1; k < 11; k++)
-    {
-        for (int j = 0; j < 127; j++)
-        {
-            line[j] = (rand() % ('z' - '0' + 1) + '0');
-        }
         printf("Your password #%02d: %s \n",k, password(line));
-    }
+    
     return 0;
 }
