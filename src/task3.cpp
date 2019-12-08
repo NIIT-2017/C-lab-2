496b87bbb9cbeeab0c8d579278cef177fb0fd10a
Mikhail Treushnikov <www.mor.ry@mail.ru>
2019-12-08 18:12:05 +0300

    Поправил task3.cpp

--- src/task3.cpp
+++ src/task3.cpp
@@ -3,8 +3,8 @@ char* layout(char buf[], int line, int count)
 {
 	for (int i = 0; i<=(2*count-1); i++)
 	{
-		if ((i>(count - line))&&(i<=((count - line)+(line - 1) * 2 + 1))) buf[i] = '*';
-		else if (i<=(count-line)) buf[i] = ' ';
+		if ((i>=(count - line))&&(i<((count - line)+(line - 1) * 2 + 1))) buf[i] = '*';
+		else if (i<(count-line)) buf[i] = ' ';
 	}
 	return buf;
 }
