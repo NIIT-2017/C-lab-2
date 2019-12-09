a7e5dbeac739184799a8f49f52fca01559de2c1f
Bear346 <57682132+Bear346@users.noreply.github.com>
2019-12-09 18:32:48 +0300

    Update main5.cpp

--- src/main5.cpp
+++ src/main5.cpp
@@ -12,7 +12,9 @@ int main()
    char line[512];
    for(int i = 0; i < 10; i++)
    {
-      password(line);
+       printf("%s\n", password(line));
+        clock_t begin = clock();
+        while (clock() < begin + CLOCKS_PER_SEC);
    }
    return 0;
 }
