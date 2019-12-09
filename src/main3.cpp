e5d0e5dd8b39e625d13eca23eca4138d8093ae84
ViktorGa <57584117+ViktorGa@users.noreply.github.com>
2019-12-09 20:42:03 +0300

    Osn

---
+++ src/main3.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "task3.h"
+#include <string.h>
+
+int main()
+{
+	char buf[200];
+	int line = 1, count = 0;
+
+	printf("goni chislo, do 21 \n");
+	scanf("%i", &count);
+	if (count <= 1)
+	{
+		printf("Kiss my BASS! \n");
+	}
+	else
+	{
+		for (line; (line <= count); line++)
+		{
+			printf("%s\n", layout(buf, line, count));
+		}
+	}
+	return 0;
+}
---
+++ src/task6.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+char * clear(char * line)
+{
+	int j = 0;
+	int i = strlen(line) - 1;
+	while (line[i] == ' ')
+	{
+		line[i] = '\0';
+		i--;
+	}
+	while (line[j] == ' ')
+	{
+		for (int a = 0; a <= (strlen(line) - 1); a++)
+		{
+			line[a] = line[a + 1];
+		}
+	}
+	while (line[j])
+	{
+		if (line[j] == ' ' && line[j + 1] == ' ')
+		{
+			for (int a = j; a <= strlen(line) - 1; a++)
+			{
+				line[a] = line[a + 1];
+			}
+		}
+		else j++;
+	}
+	return line;
+}
