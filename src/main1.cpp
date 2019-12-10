beb354249dffb0bebf4b26d18c2057af6d6ff759
Bear346 <57682132+Bear346@users.noreply.github.com>
2019-12-10 13:17:18 +0300

    Update main1.cpp

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,6 +1,7 @@
 #include "task1.h"
 #include <stdio.h>
-#include<Windows.h>
+#include <time.h>
+#include <stdlib.h>
 
 
 int main()
@@ -9,8 +10,20 @@ int main()
     int startHeight = 0, currTime=0;
     printf("Enter your Height!\n");
     scanf("%d",&startHeight);
-    float result = height(currTime,startHeight);
-    if (result ==-1)
-      printf("BABAH!!!!\n");
+    float totalHeight = startHeight;
+	while (1)
+	{
+		
+		if (totalHeight < 0)
+		{
+			printf("BABAH!!!!\n");
+			break;
+		}
+		printf("t = %d from h= %06.1f m\n",currTime,totalHeight);
+		currTime++;
+		clock_t begin = clock();
+		while (clock() < begin + CLOCKS_PER_SEC / 1);
+		totalHeight = height(currTime, startHeight);
+	}
     return 0;
 }
