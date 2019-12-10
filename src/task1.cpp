22de03bf5e7b81b9d04451fc65c8fc9ead8e15b6
Bear346 <57682132+Bear346@users.noreply.github.com>
2019-12-10 13:15:36 +0300

    Update task1.cpp

--- src/task1.cpp
+++ src/task1.cpp
@@ -1,22 +1,8 @@
 #include "task1.h"
-#include<stdio.h>
-#include <stdlib.h>
-#include<time.h>
 
 float height(int currTime, int startHeight)
 {
-   int time = 0;
-   float height = 0;
-   while(1)
-   {
-     currTime=(9.81*(time*time))/2;
-     height=(startHeight - currTime);
-     if(height < 0)
-        return -1;
-     printf("t = %d from h= %.1f m\n",time,height);
-     clock_t begin = clock();
-	  while (clock() < begin + CLOCKS_PER_SEC / 5);
-     time++;
-   }
-   return height;
+const float g = 9.81;
+float  totalHeight= (startHeight - g * (currTime*currTime) / 2);
+return totalHeight;
 }
