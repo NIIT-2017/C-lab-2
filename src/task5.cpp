72021b0b18fcfa5e55525b8a415dbfa45a3ac88b
melitzev <57032272+melitzev@users.noreply.github.com>
2019-12-10 12:09:04 +0300

    fix  quantity of numbers in line. task5

--- src/task5.cpp
+++ src/task5.cpp
@@ -6,8 +6,9 @@ char * password(char * line)
 {
     for (int i = 0;i < 8;i++){
                 line[i]= (48+rand()%(57-48+1)); // числа 48-57
+                if (i<5)
                 line[++i]= (65+rand()%(90-65+1)); // заглавные 65-90
-                if (i < 7)
+                if (i < 6)
                     line[++i]= (97+rand()%(122-97+1)); // строчные 97-122
             }
 return line;
