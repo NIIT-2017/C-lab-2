96c27d1bc849c14217f0a5da0973f62fce3f2e3e
melitzev <57032272+melitzev@users.noreply.github.com>
2019-12-10 10:24:17 +0300

    fix line and main in task3

--- src/main3.cpp
+++ src/main3.cpp
@@ -1,20 +1,18 @@
 #include <stdio.h>
 #include "task3.h"
 
-
-
 int main()
 {
     int count=0;
-    int line;
-    char buf[20];
+    int line =1;
+    char buf[256];
     printf("Введите количетсво строк стреугольника (целое число)\n");
     scanf("%d",&count);
 
     char * layout(char buf[],int line,int count);
-
-    puts(layout(buf,line,count));
-
+    for (;line <= count;line++){
+        puts(layout(buf,line,count));
+    }
 
     return 0;
 }
--- src/task3.cpp
+++ src/task3.cpp
@@ -2,22 +2,10 @@
 
 char * layout(char buf[],int line,int count)
 {
-
-    int i,j;
-    for (i=1;i<=count;i++)
-    {
-        for (j=1;j<=count+i;j++)
-        {
-            if (j<=count-i+1)
-            {
-                putchar(' ');
-            } else
-            {
-                putchar('*');
-            }
-        }
-        putchar('\n');
+    for (int j=0;j < (count + line -1);j++){
+        if (j <= (count - (line+1)))
+            buf[j] = ' ';
+        else buf[j] = '*';
     }
-
     return buf;
 }
