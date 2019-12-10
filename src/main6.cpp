ac0150e060aa40af48cff79b7cad77d9c073b5cd
melitzev <57032272+melitzev@users.noreply.github.com>
2019-12-10 15:03:43 +0300

    rewrite task6

--- src/main6.cpp
+++ src/main6.cpp
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include "task6.h"
+#include <string.h>
 
 int main()
 {
     char line[50];
     printf("Введите строку (с пробелами) \n");
     fgets(line,50,stdin);
-    char * clear(char * line);
+    if (line[strlen(line)-1] == '\n')
+    line[strlen(line)-1]=0;
+
     puts(clear(line));
 
     return 0;
--- src/task6.cpp
+++ src/task6.cpp
@@ -1,19 +1,39 @@
+#include <string.h>
+
 char * clear(char * line)
 {
-    for (int i = 0;line[i];i++){
-        if ((line[i] == ' ')&& (i == 0)) {
-            for (int j = i;line[j];j++){
-                line[j] = line[j+1];
-            }
-        }else if ((line[i] == ' ') && (line[++i] == ' ')){
-                for(int k = i;line[k];k++){
-                    line[k] = line[k+1];
+    int flag = 0;
+    int lfirst =0;
+    if (line[0] ==' ') {
+        for (int i = 0; line[i]; i++) {
+            if (line[i] != ' '){
+                lfirst = i;
+                break;
                 }
-            }
-        else if ((line[i] == ' ') && (line[++i] == '\n')){
-                line[i] = '\n';
+        }for (int j = 0;line[j];j++){
+            line[j] = line[j+lfirst];
         }
+    }
+    for (int k =0;line[k];k++){
+        if (line[k] == ' ')
+            flag = 1;
+        for (int l = k+1; line[l];l++){
+               if ((line[l] != ' ') && (line[l+1] !='\n')) {
+                   flag = 0;
+                   break;
+               } else if ((line[l]== ' ')&& (flag == 1)){
+                   for (int m = l;line[m];m++ ){
+                       line[m] = line[m+1];
+                   }
+                   k=k-1;
+               }  else if ((line[l] == ' ') && (line[l+1] ==0)){
+                   line[l-1] = 0;
+               }
+
+
+           }
 
     }
+
     return line;
 }
