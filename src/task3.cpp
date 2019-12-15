c39dd6e2f4fa6d1a42a3a2badc6b3afd8c1d50e7
melitzev <57032272+melitzev@users.noreply.github.com>
2019-12-15 20:01:02 +0300

    add '\0' in the end of line.

--- src/task3.cpp
+++ src/task3.cpp
@@ -2,10 +2,12 @@
 
 char * layout(char buf[],int line,int count)
 {
-    for (int j=0;j < (count + line -1);j++){
+    int j;
+    for (j=0;j < (count + line -1);j++){
         if (j <= (count - (line+1)))
             buf[j] = ' ';
         else buf[j] = '*';
     }
+    buf[j] = '\0';
     return buf;
 }
