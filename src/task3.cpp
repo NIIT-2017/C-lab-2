0c5e178b8108611624ec0f9832f8843748597776
EveerDrof <EveerDrof@git.com>
2019-12-21 19:16:04 +0300

    Fifth commit

--- src/task3.cpp
+++ src/task3.cpp
@@ -1,13 +1,13 @@
 char* layout(char buf[], int line, int count)
 {
-    for (int i = 0; i<=count; i++)
+    for (int i = 0; i<count; i++)
     {
-        if (i>count-(line*2-1))
+        if (i>=count-(line*2-1))
             buf[i] = '*';
         else
             buf[i] = ' ';
 
     }
-    buf[count+1] = '\0';
+    buf[count] = '\0';
     return buf;
 }
