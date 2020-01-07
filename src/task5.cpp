7da0c9c2205795308a728cdeb1b70d1af0ed7e3b
SvetlanaKov <lanaiks2404@gmail.com>
2020-01-07 23:57:50 +0300

    C-lab#2

---
+++ src/task5.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "task5.h"
+
+char* password(char* line)
+{
+	
+	for (int i = 0; i < 8; i++)
+
+	{
+		int random = rand() % 3 + 1;
+		if (random == 1)
+
+			line[i] = '0' + rand() % ('9' - '0' + 1);
+		else if (random == 2)
+
+			line[i] = 'A' + rand() % ('Z' - 'A' + 1);
+
+		else
+			line[i] = 'a' + rand() % ('z' - 'a' + 1);
+	}
+	
+return line;
+}
