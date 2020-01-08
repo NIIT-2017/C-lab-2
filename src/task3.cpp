949e60008acd9eac92981dc5eb08b93a6ebd62bb
AlexIvan-Shulc <57383446+AlexIvan-Shulc@users.noreply.github.com>
2020-01-08 19:47:41 +0300

    Update task3.cpp

--- src/task3.cpp
+++ src/task3.cpp
@@ -4,10 +4,10 @@
 char* layout(char buf[], int line, int count)
 {
 	
-	int j = 0;
-	int i = 0;
+	int j;
+	int i;
 		
-		for (i = 1; i <= count - line; i++) 
+		for (i = 0; i <= count - line; i++) 
 			buf[i] = ' ';
 		for (j = 1; j < line * 2; j++) 
 			buf[i++] = '*';
