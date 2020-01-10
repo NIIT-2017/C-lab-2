c09798c6024b18f33faaed23687cb95a72bdfef0
AlexIvan-Shulc <57383446+AlexIvan-Shulc@users.noreply.github.com>
2020-01-10 20:52:24 +0300

    Update task3.cpp

--- src/task3.cpp
+++ src/task3.cpp
@@ -8,10 +8,14 @@ char* layout(char buf[], int line, int count)
 	int i;
 		
 		for (i = 0; i <= count - line; i++) 
+		{
 			buf[i] = ' ';
+		}
 		for (j = 1; j < line * 2; j++) 
+		{
 			buf[i++] = '*';
 			buf[i] = '\n';
+		}
 
 	return buf;
 	
