1b004943e9e86f9f65066f5b06381b381c18d075
SvetlanaKov <lanaiks2404@gmail.com>
2020-01-18 18:52:22 +0300

    new

---
+++ src/task3.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "task3.h"
+
+char* layout(char buf[], int line, int count)
+{
+	int gaps= (count - line)+1;
+    char Gap = ' ';
+    int stars = line * 2 - 1;
+    char Star = '*';
+   
+    for (int i = 0; i < ((gaps + stars)-1); i++)
+    {
+        if (i < gaps)
+            buf[i] = Gap;
+        else
+            buf[i] = Star;
+    }
+    buf[(gaps + stars) - 2] = '\0';
+	return buf;
+}
