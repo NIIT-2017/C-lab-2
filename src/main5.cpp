85d0f3952001822ead0e9bd2c8768a2fbf873515
SvetlanaKov <lanaiks2404@gmail.com>
2020-01-18 18:40:40 +0300

    new

---
+++ src/main5.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include "task5.h"
+
+int main()
+{
+	time_t now;
+	time(&now);
+	srand(now);
+	rand();
+
+	char line[9] = { 0 };
+	
+	printf("10 passwords: \n");
+	for (int i = 0; i < 10; i++)
+		printf("%s\n", password(line));
+	return 0;
+}
