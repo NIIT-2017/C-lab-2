048823bc0360711966964370e7f4e2a3f17f8c5d
Nikita <zero.echelon@mail.ru>
2020-02-28 12:16:42 +0300

    lab_2

--- src/task3.cpp
+++ src/task3.cpp
@@ -7,12 +7,13 @@ char * layout(char buf[], int line, int count)
 	int i = 0;
 	int j = 0;
 	for (i = 0; i < count - line; i++)
-	{
-		buf[i] = ' ';
-	}
+		{
+			buf[i] = ' ';
+		}
 	for (j = 0; j < 2 * line - 1; j++)
-	{
-		buf[j + i] = '*';
-	}
+		{
+			buf[j + i] = '*';
+		}
+	buf[j + i] = '\0';
 	return buf;
 }
