d243caf648d7875551fb1cceb83b5e05509f544a
Sergey Gorin <studenci@mail.ru>
2020-03-19 11:08:10 +0300

    изменения

--- src/task1.cpp
+++ src/task1.cpp
@@ -2,12 +2,10 @@
 #define g 9.81
 
 float height(int currTime, int startHeight)
-
-
 {
 	float L; 
 
-
-	L = (g * currTime * currTime) / 2; //пройденный путь
-	return startHeight- L;//осталось до земли
+	L = (g * currTime * currTime) / 2;//пройденный путь
+	startHeight = startHeight - L;
+	return startHeight;//осталось до земли
 }
