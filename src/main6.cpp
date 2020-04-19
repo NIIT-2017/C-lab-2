4da822f8378ce51bfc2baf92922e5d12a3a67036
Sergey Gorin <studenci@mail.ru>
2020-04-19 19:00:13 +0600

    изменения

--- src/main6.cpp
+++ src/main6.cpp
@@ -12,25 +12,7 @@ int main()
 	printf("Ведите  строку \n");
 	fgets(line, 255, stdin);
 	
-	int i = 0, j = 0, l = 0;
-	int len = strlen(line);
-	if (line[len - 1] == '\n')
-		line[len - 1] = '\0';
-
-	while (line[0] == ' ')
-	{
-		for ( j = 0; j <= strlen(line) - 1; j++)
-		{
-			line[j] = line[j + 1];
-		}
-	}
-	 l = strlen(line) - 1;
-	while (line[l] == ' ')
-	{
-		line[l] = '\0';
-		l--;
-	}
-	 i = 0;	
+	
 	
 	fputs(clear(line), stdout);
 	return 0;
--- src/task6.cpp
+++ src/task6.cpp
@@ -4,8 +4,27 @@
 
 char* clear(char* line)
 {
-	int i = 0, j = 0;
+	int i = 0, j = 0, l = 0;
 	int len = strlen(line);
+	if (line[len - 1] == '\n')
+		line[len - 1] = '\0';
+
+	while (line[0] == ' ')
+	{
+		for (j = 0; j <= strlen(line) - 1; j++)
+		{
+			line[j] = line[j + 1];
+		}
+	}
+	l = strlen(line) - 1;
+	while (line[l] == ' ')
+	{
+		line[l] = '\0';
+		l--;
+	}
+	i = 0;
+	//int i = 0, j = 0;
+	//int len = strlen(line);
 	while (line[i])
 	{
 		if ((line[i] == ' ') && (line[i + 1] == ' '))
